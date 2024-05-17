3428be6114962b5b22c0687ff765423641bbdb2d
TrucKhang <truckhang181001@gmail.com>
2024-05-17 08:24:43 +0700

    add tem index

--- src/sections/@dashboard/invoice/list/InvoicePrintRow.js
+++ src/sections/@dashboard/invoice/list/InvoicePrintRow.js
@@ -14,37 +14,42 @@ export const InvoicePrintRow = React.forwardRef(({ row }, ref) => {
             })
     }, [])
 
+    let curItem = 0
+
     return (
         <div ref={ref} className="grabfood-label-container" style={{ display: 'none' }}>
-            {orderDetail != null && orderDetail.items.map((item) => (
-                Array.from({ length: item.quantity }, (_, i) => (
-                    <div>
-                        <div className="grabfood-label" key={item.id}>
-                            <div>
-                                <div className="grabfood-label-id">
-                                    <b>{orderDetail.displayId}</b>
-                                </div>
-                                <div className="grabfood-label-header">
-                                    <b>{item.name}</b>
-                                </div>
-                                <div className="grabfood-label-modifiers">
-                                    {item.modifierGroups.map((modifierGroup) => (
-                                        modifierGroup.modifiers.map((modifierSelected) => (
-                                            <div className="grabfood-label-modifier">
-                                                + {modifierSelected.modifierName}
-                                            </div>
-                                        ))))}
-                                </div>
-                                <div className="grabfood-label-price-time">
-                                    <b>{item.fare.priceDisplay / item.quantity}đ - {fDateTime(orderDetail.time.createdAt)}</b>
+            {orderDetail != null && orderDetail.items.map((item, totalIndex) => (
+                Array.from({ length: item.quantity }, (_, i) => {
+                    curItem += 1
+                    return (
+                        <div>
+                            <div className="grabfood-label" key={item.id}>
+                                <div>
+                                    <div className="grabfood-label-id">
+                                        <b>{orderDetail.displayId}</b>
+                                    </div>
+                                    <div className="grabfood-label-header">
+                                        <b>{item.name}</b>
+                                    </div>
+                                    <div className="grabfood-label-modifiers">
+                                        {item.modifierGroups.map((modifierGroup) => (
+                                            modifierGroup.modifiers.map((modifierSelected) => (
+                                                <div className="grabfood-label-modifier">
+                                                    + {modifierSelected.modifierName}
+                                                </div>
+                                            ))))}
+                                    </div>
+                                    <div className="grabfood-label-price-time">
+                                        <b>{item.fare.priceDisplay / item.quantity}đ - {fDateTime(orderDetail.time.createdAt)}</b>
+                                    </div>
                                 </div>
                             </div>
+                            {(curItem % 2) !== 0 && (<div className="grabfood-label-empty">
+                                <b>holder</b>
+                            </div>)}
                         </div>
-                        <div className="grabfood-label-empty">
-                            <b>holder</b>
-                        </div>
-                    </div>
-                ))
+                    )
+                })
             ))}
         </div>
     );
